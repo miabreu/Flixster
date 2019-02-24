ec213152f068aa6b1f07d42ea85e31b37c6ab3b1
Michelle Abreu <mabreu177@gmail.com>
2019-02-23 21:51:06 -0500

    Initial Commit

---
+++ Flixster/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Flixster
+//
+//  Created by Michelle Abreu on 2/23/19.
+//  Copyright © 2019 Michelle Abreu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
