03d03f21387560fcec7e9825d709f98b07a8cffe
Michelle Abreu <mabreu177@gmail.com>
2019-02-28 22:24:03 -0500

    Flixster 2.0

---
+++ Flixster/MovieGridCell.swift
@@ -0,0 +1,15 @@
+//
+//  MovieGridCell.swift
+//  Flixster
+//
+//  Created by Michelle Abreu on 2/28/19.
+//  Copyright © 2019 Michelle Abreu. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
+
---
+++ Flixster/MoviesDetailsViewController.swift
@@ -0,0 +1,41 @@
+//
+//  MoviesDetailsViewController.swift
+//  Flixster
+//
+//  Created by Michelle Abreu on 2/28/19.
+//  Copyright © 2019 Michelle Abreu. All rights reserved.
+//
+
+import UIKit
+import AlamofireImage
+
+class MovieDetailsViewController: UIViewController {
+    
+    @IBOutlet weak var backdropView: UIImageView!
+    @IBOutlet weak var posterView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var synopsisLabel: UILabel!
+    
+    var movie: [String: Any]! = [:]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        titleLabel.text = movie["title"] as? String
+        synopsisLabel.text = movie["overview"] as? String
+        titleLabel.sizeToFit()
+        synopsisLabel.sizeToFit() // vertically align
+        
+        let baseUrl = "https://image.tmdb.org/t/p/w300"
+        let posterPath = movie["poster_path"] as! String
+        let posterUrl = URL(string: baseUrl + posterPath)
+        
+        let backdropPath = movie["backdrop_path"] as! String
+        let backdropUrl = URL(string: "https://image.tmdb.org/t/p/w780" + backdropPath)
+        
+        posterView.af_setImage(withURL: posterUrl!)
+        backdropView.af_setImage(withURL: backdropUrl!)
+        
+    }
+}
+
